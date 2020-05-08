8cb280d5bd0848aa45bb69f8f7392e5b4a526204
kiku ishimoto <kikuish@gmail.com>
2020-05-08 17:20:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ end
 
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
-    holiday.map do |holiday, supllies|
+    holiday.map do |holiday, supplies|
       holiday if supplies.include?('BBQ')
     end
   end
